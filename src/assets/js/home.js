15174dfd0d37e0a7198856bc295604b1f3f48d5d
amrahman90 <mahmud.aero@gmail.com>
2018-07-29 00:02:06 +0530

    get results for 3 categories

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -2,6 +2,9 @@
 
 let freqPoints = new Array(30);
 let gradPoints = new Array(30);
+let emotionalQues = [2,4,8,11,14,16,18,20,22,24];
+let vegetativeQues = [1,5,7,9,15,19,23,26,27,28];
+let motorischeQues = [3,6,10,12,13,17,21,25,29,30];
 
 let totalPoints;
 
@@ -70,6 +73,86 @@ let initializeVariables = () => {
         gradPoints[i] = 0;
     }
 };
+
+let checked = (ev) => {
+    let q = ev.id.split('_')[0];
+    let quesId = q.substring(0, q.length-1);
+    let quesType = q.substring(quesId.length, q.length);
+    let quesPoint = ev.id.split('_')[1];
+
+    // toggle check icons
+    for(let i=1; i<=4; i++) {
+        $("#"+quesId+quesType+"_"+i).find("i").remove();
+    }
+    $("#"+ev.id).append("<i class='fas fa-check'></i>");
+
+    // activate the get results button
+    if(document.getElementsByTagName("i").length == 60) {
+        $('#result-button').prop("disabled", false);
+    }
+
+    // calculate points
+    if (quesType === 'f' || quesType === 'F') {
+        freqPoints[quesId - 1] = parseInt(quesPoint);
+    }
+    else if (quesType === 'g' || quesType === 'G') {
+        gradPoints[quesId - 1] = parseInt(quesPoint);
+    }
+    $("td#" + quesId + "_9").html(freqPoints[quesId - 1] + gradPoints[quesId - 1]);
+}
+
+let getresults = () => {
+    let emotionalPoints = 0;
+    let vegetativePoints = 0;
+    let motorischePoints = 0;
+    for(let i=1; i<=30; i++) {
+        if(emotionalQues.indexOf(i) > -1) {
+            emotionalPoints += (freqPoints[i-1] + gradPoints[i-1]);
+        }
+        else if(vegetativeQues.indexOf(i) > -1) {
+            vegetativePoints += (freqPoints[i-1] + gradPoints[i-1]);
+        }
+        else if(motorischeQues.indexOf(i) > -1) {
+            motorischePoints += (freqPoints[i-1] + gradPoints[i-1]);
+        }
+    }
+    $("div#result").show('slow');
+
+    //show emotional result
+    if(emotionalPoints > 40) {
+        $("p#firstLevelResult-3").show('slow');
+    }
+    else if(emotionalPoints > 30) {
+        $("p#firstLevelResult-2").show('slow');
+    }
+    else if(emotionalPoints > 19) {
+        $("p#firstLevelResult-1").show('slow');
+    }
+
+    //show vegetative result
+    if(vegetativePoints > 40) {
+        $("p#secondLevelResult-3").show('slow');
+    }
+    else if(vegetativePoints > 30) {
+        $("p#secondLevelResult-2").show('slow');
+    }
+    else if(vegetativePoints > 19) {
+        $("p#secondLevelResult-1").show('slow');
+    }
+
+    //show motorische result
+    if(motorischePoints > 40) {
+        $("p#thirdLevelResult-3").show('slow');
+    }
+    else if(motorischePoints > 30) {
+        $("p#thirdLevelResult-2").show('slow');
+    }
+    else if(motorischePoints > 19) {
+        $("p#thirdLevelResult-1").show('slow');
+    }
+
+    $('#result-button').prop("disabled", true);
+}
 
 $(document).ready(() => {
     initializeVariables();
